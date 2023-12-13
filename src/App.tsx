e3312bd15a5cc456a1d658bb14ae3dad9de8bf5f
mate1225 <mate981225@gmail.com>
2023-12-13 14:31:48 +0100

    add: initial files

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+//react router
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+//tsx
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <div className=" flex gap-5">
+          <Link to={"/"} className="rounded-lg bg-red-500 p-3">
+            Home
+          </Link>
+          <Link to={"/Booking"} className="rounded-lg bg-red-500 p-3">
+            Booking
+          </Link>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Booking" element={<Booking />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
