af16f355aa587a2400399679bae7de1749e91409
mate1225 <mate981225@gmail.com>
2024-01-03 06:57:56 +0100

    bug fix: UI

--- src/assets/components/Introductions.tsx
+++ src/assets/components/Introductions.tsx
@@ -14,7 +14,7 @@ export default function Introductions(props: {
       >
         <img src={img} className="mb-[3.5rem]" />
         <img src={imagesDecoration} className=" mb-[2.25rem] w-[4.4375rem]" />
-        <h2 className=" mx-[4rem]  mb-[0.81rem] text-[2rem] font-bold leading-[2.5rem]">
+        <h2 className=" mx-[3.5rem]  mb-[0.81rem] text-[2rem] font-bold leading-[2.5rem]">
           {title}
         </h2>
         <p className=" mx-6">{text}</p>
